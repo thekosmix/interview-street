7575a6acf4a2fcc77cec8c2f4fc6d760005a57c6
Siddharth Kumar <siddharth.dce08@gmail.com>
2016-05-15 01:29:42 +0530

    DB config file

--- oprp/_php/config.php
+++ oprp/_php/config.php
@@ -2,8 +2,8 @@
 error_reporting(E_ERROR);
 defined('DB_SERVER') ? null : define("DB_SERVER","localhost");
 defined('DB_NAME') ? null : define("DB_NAME","oprp");
-defined('DB_USER') ? null : define("DB_USER","root");
-defined('DB_PASS') ? null : define("DB_PASS","vinay");
+defined('DB_USER') ? null : define("DB_USER","YOUR_MYSQL_USER");
+defined('DB_PASS') ? null : define("DB_PASS","YOUR_MYSQL_PASSWORD");
 defined('ABS_PATH') ? null : define("ABS_PATH","http://localhost/oprp/");
  
 ?>
