5ffff888dee8141101ad13d04c745cf5e44ee49b
Siddharth Kumar <siddharth.dce08@gmail.com>
2020-11-20 18:10:37 +0530

    Update config.php

--- oprp/_php/config.php
+++ oprp/_php/config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ERROR);
-defined('DB_SERVER') ? null : define("DB_SERVER","van-rakshak.cgnps0bsymw3.ap-south-1.rds.amazonaws.com");
+defined('DB_SERVER') ? null : define("DB_SERVER","HOST");
 defined('DB_NAME') ? null : define("DB_NAME","oprp");
 defined('DB_USER') ? null : define("DB_USER","root");
-defined('DB_PASS') ? null : define("DB_PASS","One!Rakshak");
+defined('DB_PASS') ? null : define("DB_PASS","PASS");
 defined('ABS_PATH') ? null : define("ABS_PATH","http://52.66.249.196/oprp/");
  
 ?>
