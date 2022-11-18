41dba0ebaff5e7295c2df0c46b2b718603739cf9
Siddharth Kumar <siddharthkumar@urbancompany.com>
2022-11-18 20:29:49 +0530

    changed home address url to fit with any host

--- oprp/_php/config.php
+++ oprp/_php/config.php
@@ -5,6 +5,6 @@ defined('DB_SERVER') ? null : define("DB_SERVER","localhost");
 defined('DB_NAME') ? null : define("DB_NAME","oprp");
 defined('DB_USER') ? null : define("DB_USER","root");
 defined('DB_PASS') ? null : define("DB_PASS","root");
-defined('ABS_PATH') ? null : define("ABS_PATH","http://localhost/");
+defined('ABS_PATH') ? null : define("ABS_PATH","/");
  
 ?>
